ab67672c56d32c92aa5e28481ae6895da9e65590
Murat Han Acıpayam <acipayammurathan@gmail.com>
2021-01-06 17:12:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace RaspberryGPIOControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Semih ! ");
+
+            int pin = 4;
+
+            GpioController controller = new GpioController();
+            controller.OpenPin(pin, PinMode.Output);
+
+
+            while (true)
+            {
+                controller.Write(pin, PinValue.High);
+                Thread.Sleep(1000);
+                controller.Write(pin, PinValue.Low);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
